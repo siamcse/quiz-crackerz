5e4650a3c465af91fdf1bc2a42be8b633ea4b966
Abu Siam <siamcse01@gmail.com>
2022-10-10 23:10:04 +0600

    topic added

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Header = () => {
     return (
-        <div className="px-4 py-16 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-20">
+        <div className="px-4 pt-16 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:pt-20">
       <div className="max-w-xl mb-10 md:mx-auto sm:text-center lg:max-w-2xl md:mb-12">
         <div>
           <p className="inline-block px-3 py-px mb-4 text-xs font-semibold tracking-wider text-teal-900 uppercase rounded-full bg-teal-accent-400">
@@ -41,80 +41,6 @@ const Header = () => {
           Sed ut perspiciatis unde omnis iste natus error sit voluptatem
           accusantium doloremque rem aperiam, eaque ipsa quae.
         </p>
-      </div>
-      <div className="grid max-w-sm gap-5 mb-8 lg:grid-cols-3 sm:mx-auto lg:max-w-full">
-        <div className="px-10 py-20 text-center border rounded lg:px-5 lg:py-10 xl:py-20">
-          <p className="mb-2 text-xs font-semibold tracking-wide text-gray-600 uppercase">
-            20 Nov 2020
-          </p>
-          <a
-            href="/"
-            className="inline-block max-w-xs mx-auto mb-3 text-2xl font-extrabold leading-7 transition-colors duration-200 hover:text-deep-purple-accent-400"
-            aria-label="Read article"
-            title="Nori grape silver beet broccoli kombu beet"
-          >
-            Nori grape silver beet broccoli kombu beet
-          </a>
-          <p className="max-w-xs mx-auto mb-2 text-gray-700">
-            Sed ut perspiciatis unde omnis iste natus error sit voluptatem
-            accusantium doloremque.
-          </p>
-          <a
-            href="/"
-            aria-label=""
-            className="inline-flex items-center font-semibold transition-colors duration-200 text-deep-purple-accent-400 hover:text-deep-purple-800"
-          >
-            Read more
-          </a>
-        </div>
-        <div className="px-10 py-20 text-center border rounded lg:px-5 lg:py-10 xl:py-20">
-          <p className="mb-2 text-xs font-semibold tracking-wide text-gray-600 uppercase">
-            20 Nov 2020
-          </p>
-          <a
-            href="/"
-            className="inline-block max-w-xs mx-auto mb-3 text-2xl font-extrabold leading-7 transition-colors duration-200 hover:text-deep-purple-accent-400"
-            aria-label="Read article"
-            title="Well, the way they make shows is, they make one"
-          >
-            Well, the way they make shows is, they make one
-          </a>
-          <p className="max-w-xs mx-auto mb-2 text-gray-700">
-            Some pilots get picked and become television programs. Some don't,
-            become nothing.
-          </p>
-          <a
-            href="/"
-            aria-label=""
-            className="inline-flex items-center font-semibold transition-colors duration-200 text-deep-purple-accent-400 hover:text-deep-purple-800"
-          >
-            Read more
-          </a>
-        </div>
-        <div className="px-10 py-20 text-center border rounded lg:px-5 lg:py-10 xl:py-20">
-          <p className="mb-2 text-xs font-semibold tracking-wide text-gray-600 uppercase">
-            20 Nov 2020
-          </p>
-          <a
-            href="/"
-            className="inline-block max-w-xs mx-auto mb-3 text-2xl font-extrabold leading-7 transition-colors duration-200 hover:text-deep-purple-accent-400"
-            aria-label="Read article"
-            title="Pommy ipsum smeg head whizz morris himer due"
-          >
-            Pommy ipsum smeg head whizz morris himer due
-          </a>
-          <p className="max-w-xs mx-auto mb-2 text-gray-700">
-            Taking the mick middle className bog roll bow ties are cool posh
-            nosh off t'shop, stew and dumps.
-          </p>
-          <a
-            href="/"
-            aria-label=""
-            className="inline-flex items-center font-semibold transition-colors duration-200 text-deep-purple-accent-400 hover:text-deep-purple-800"
-          >
-            Read more
-          </a>
-        </div>
       </div>
     </div>
     );
--- src/components/Topics/Topics.js
+++ src/components/Topics/Topics.js
@@ -9,12 +9,16 @@ const Topics = () => {
     return (
         <div>
             <Header></Header>
-            {
-                topics.map(topic=> <Topic 
-                    key={topic.id}
-                    topic={topic}
-                />)
-            }
+            <div className="px-4 pb-16 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:pb-20">
+                <div className="grid gap-8 lg:grid-cols-4 sm:max-w-sm sm:mx-auto lg:max-w-full">
+                    {
+                        topics.map(topic=> <Topic 
+                            key={topic.id}
+                            topic={topic}
+                        />)
+                    }
+                </div>
+            </div>
         </div>
     );
 };
