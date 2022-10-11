552b3a8ad21aa9a7bb959c2a62bd0dd55ee15ec5
Abu Siam <siamcse01@gmail.com>
2022-10-11 16:00:39 +0600

    correct ans show added

--- src/components/Question/Question.js
+++ src/components/Question/Question.js
@@ -1,28 +1,57 @@
-import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faEye, faEyeSlash } from '@fortawesome/free-solid-svg-icons';
+import React, { useState } from 'react';
 import Option from '../Option/Option';
+import { Alert } from '@mui/material';
 
-const Question = ({qs,index}) => {
+const Question = ({qs,index,handleShowAns,handleAns,quizAns}) => {
     const {id,options,question,correctAnswer} =qs;
+    const [show, setShow] = useState(false);
+    const handleAnsView = ()=>{
+        setShow(current=>!current);
+    }
+    
     return (
         <div className="px-4 py-16 mx-auto sm:max-w-xl md:max-w-full lg:max-w-2xl md:px-24 lg:px-8 lg:py-20">
             <div>
                 <div className="p-8 bg-cyan-400 border rounded shadow-sm">
+                <div className='flex justify-between items-start gap-6'>
+                    <p className="inline-block mb-3 text-2xl font-bold text-black">
+                    Quiz {index+1}: {question.slice(3,-4)}
+                    </p>
+                    <button onClick={()=>handleShowAns(id)} className=''>
+                        <FontAwesomeIcon onClick={handleAnsView} className={show?'hidden':''} icon={faEye}/>
+                        <FontAwesomeIcon onClick={handleAnsView} className={show?'':'hidden'} icon={faEyeSlash}/>
+                    </button>
+                </div>
                 
-                <p className="inline-block mb-3 text-2xl font-bold text-black">
-                    Quiz {index+1}: {question}
-                </p>
                 <p className="mb-5 text-white bg-gray grid grid-cols-2 gap-10">
                     {
                         options.map((option,index)=><Option
                             option={option}
                             key={index}
+                            handleAns={handleAns}
+                            correctAnswer={correctAnswer}
                         />)
                     }
+                    
                 </p>
-                
+                <div className={show ? '' : 'hidden'}>
+                <Alert
+                    color="success"
+                    onDismiss={function onDismiss() { handleAnsView() }}>
+                    <span>
+                        <span className="font-medium">
+                            Correct Answer :
+                        </span>
+                        {' '}{quizAns}
+                    </span>
+                </Alert>
+            </div>
                 </div>
                 
             </div>
+            
         </div>
     );
 };
--- src/components/Quiz/Quiz.js
+++ src/components/Quiz/Quiz.js
@@ -1,11 +1,27 @@
-import React from 'react';
+import { Alert } from '@mui/material';
+import React, { useState } from 'react';
 import { useLoaderData } from 'react-router-dom';
 import Question from '../Question/Question';
 
 const Quiz = () => {
     const quiz = useLoaderData().data;
     const {id,name,questions,total} = quiz;
-    console.log(quiz);
+    console.log(questions);
+    const [quizAns, setQuizAns] = useState('');
+    const handleAns = (option,correctAnswer)=>{
+        console.log(option);
+        console.log(correctAnswer);
+        if(correctAnswer===option){
+            alert('Correct ans')
+        }
+        else{
+            <Alert severity="error">This is an error alert — check it out!</Alert>
+        }
+    }
+    const handleShowAns = (id) =>{
+        const selectQues = questions.find(q=>q.id===id);
+        setQuizAns(selectQues.correctAnswer);    
+    }
     return (
         <div>
             <h2 className='text-3xl text-blue-400'>Quiz of {name}</h2>
@@ -14,6 +30,9 @@ const Quiz = () => {
                     key={qs.id}
                     qs={qs}
                     index={index}
+                    handleShowAns={handleShowAns}
+                    quizAns={quizAns}
+                    handleAns={handleAns}
                 />)
             }
         </div>
